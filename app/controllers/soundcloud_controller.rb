ec58d36e46df5e8dac3e184224e3d6d0239b53b5
danieldumycz <daniel.dumycz@outlook.com>
2016-12-06 13:35:17 +1100

    General Cleanup

--- app/controllers/soundcloud_controller.rb
+++ app/controllers/soundcloud_controller.rb
@@ -3,8 +3,8 @@ class SoundcloudController < ApplicationController
 
   def connect
   	# create client object with app credentials
-		client = Soundcloud.new(:client_id => "21097f8dc77f5d34bff0633f46ee5e99",
-		                        :client_secret => "9a2f08325d439ebd4ceae6c071ca1f10",
+		client = Soundcloud.new(:client_id => ENV["SOUNDCLOUD_CLIENT_ID"],
+		                        :client_secret => ENV["SOUNDCLOUD_CLIENT_SECRET"],
 		                        :redirect_uri => "http://localhost:3000/soundcloud/oauth-callback",
 		                        :response_type => 'code')
 
@@ -14,8 +14,8 @@ class SoundcloudController < ApplicationController
 
 	def connected
 		# create client object with app credentials
-		client = Soundcloud.new(:client_id => "21097f8dc77f5d34bff0633f46ee5e99",
-		                    :client_secret => "9a2f08325d439ebd4ceae6c071ca1f10",
+		client = Soundcloud.new(:client_id => ENV["SOUNDCLOUD_CLIENT_ID"],
+		                    :client_secret => ENV["SOUNDCLOUD_CLIENT_SECRET"],
 		                    :redirect_uri => "http://localhost:3000/soundcloud/oauth-callback")
 		# exchange authorization code for access token
 		access_token = client.exchange_token(:code => params[:code])
@@ -30,8 +30,8 @@ class SoundcloudController < ApplicationController
 
 	def refresh
 		# create client object with app credentials and refresh token
-		client = Soundcloud.new(:client_id => '21097f8dc77f5d34bff0633f46ee5e99',
-		                        :client_secret => '9a2f08325d439ebd4ceae6c071ca1f10',
+		client = Soundcloud.new(:client_id => ENV["SOUNDCLOUD_CLIENT_ID"],
+		                        :client_secret => ENV["SOUNDCLOUD_CLIENT_SECRET"],
 		                        :refresh_token => 'SOME_REFRESH_TOKEN')
 
 		# the client can now be used to make authenticated API calls
