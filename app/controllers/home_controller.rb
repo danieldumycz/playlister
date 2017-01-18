efd77da7a99bb34727ed82ff40ea7aad71f1f37c
danieldumycz <daniel.dumycz@outlook.com>
2017-01-18 16:40:41 +1100

    Identities are now sourced from the current logged in user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,21 @@
 class HomeController < ApplicationController
+	before_action :authenticate_user!
+	before_action :set_spotify
+	before_action :set_soundcloud
+
   def index
   	if(params.has_key?(:access_token))
   		@access_token = params[:access_token]
-  		soundcloud = Identity.where(provider: 'soundcloud').first
-  		soundcloud.save_soundcloud_token(@access_token)
+  		@soundcloud.save_soundcloud_token(@access_token)
   	end
   end
+
+  def set_spotify
+  	@spotify = current_user.identities.where(provider:'spotify').first
+	end
+
+  def set_soundcloud
+  	@soundcloud = current_user.identities.where(provider:'soundcloud').first
+	end
+
 end
