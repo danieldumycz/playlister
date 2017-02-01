ff2a0da9cafa61c578af1556014e4929ea7f0568
danieldumycz <daniel.dumycz@outlook.com>
2017-02-01 23:24:12 +1100

    UI changes represent artist/poster for any given track

--- app/assets/javascripts/components/soundcloud_track.es6.jsx
+++ app/assets/javascripts/components/soundcloud_track.es6.jsx
@@ -16,9 +16,16 @@ class SoundcloudTrack extends React.Component {
   }
 
   render () {
+  	let user = null;
+  	if (this.props.track.user.username){
+  		user = "Poster: " + this.props.track.user.username;
+  	} else {
+  		user = "ID: " + this.props.track.user.id;
+
+  	}
     return (
     	<div className="small_card">
-	    		{this.props.track.title}
+	    		{this.props.track.title} <sub className="right_align">{user}</sub>
           {this.state.clicked ? <div className="cursorPointer" onClick={this.onClick}>Hide Search</div> : <div className="cursorPointer" onClick={this.onClick}>Search</div>  }
 	        {this.state.clicked ? 
 	          <SpotifySearchContainer title={this.props.track.title} />
--- app/assets/javascripts/components/spotify_track.es6.jsx
+++ app/assets/javascripts/components/spotify_track.es6.jsx
@@ -1,10 +1,11 @@
 class SpotifyTrack extends React.Component {
   render () {
+  	console.log(this.props.track);
   	let track = null;
-  	if(!this.props.search){
-  		track = this.props.track.name;
+  	if(this.props.track.artists.length > 1){
+  		track = this.props.track.artists[0].name + "  (" + this.props.track.artists.length + " more) | " + this.props.track.name; 
   	} else {
-  		track = this.props.track.name + " by " + this.props.track.artists[0].name; 
+  		track = this.props.track.artists[0].name + " | " + this.props.track.name; 
   	}
     return (
     	<div className="small_card">
